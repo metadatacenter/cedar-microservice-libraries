e552f12a8b13cd04ab65104f5ae9ca0f5eb9439d
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2024-06-28 01:37:45 -0700

    Remove sout, make some queries more readable

--- cedar-workspace-operations-library/src/main/java/org/metadatacenter/server/neo4j/cypher/query/CypherQueryBuilderVersion.java
+++ cedar-workspace-operations-library/src/main/java/org/metadatacenter/server/neo4j/cypher/query/CypherQueryBuilderVersion.java
@@ -3,8 +3,9 @@ package org.metadatacenter.server.neo4j.cypher.query;
 public class CypherQueryBuilderVersion extends AbstractCypherQueryBuilder {
 
   public static String getResourceWithPreviousVersion() {
-    return "" +
-        " MATCH (artifact:<LABEL.ARTIFACT> {<PROP.PREVIOUS_VERSION>:{<PH.ID>}})" +
-        " RETURN artifact";
+    return """
+        MATCH (artifact:<LABEL.ARTIFACT> {<PROP.PREVIOUS_VERSION>:{<PH.ID>}})
+        RETURN artifact
+        """;
   }
 }
